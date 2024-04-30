dcac0cc7cb7e2d59696c2be262052840e374882a
4Flat <shinie4132@naver.com>
2024-05-01 02:00:13 +0900

    add top bar

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,4 +1,18 @@
 <template>
+  <body
+    topmargin="0"
+    bottommargin="0"
+    leftmargin="0"
+    rightmargin="0"
+    style="background-color: #f6f6f6"
+  >
+    <div class="menu">
+      <nav class="clearfix">
+        <ul class="clearfix"></ul>
+        <a id="pull" href="#"></a>
+      </nav>
+    </div>
+  </body>
   <div>
     <iframe src="http://localhost:5000/video" width="720" height="480" />
   </div>
@@ -48,4 +62,71 @@ export default {
   },
 };
 </script>
-<style></style>
+<style>
+nav {
+  font-size: 12pt;
+  font-family: "PT Sans", Arial, Sans-serif;
+  position: relative;
+}
+nav ul {
+  padding: 0;
+  margin: 0 auto;
+  width: auto;
+}
+nav a {
+  line-height: 50px;
+  height: 50px;
+}
+nav li a {
+  box-sizing: border-box;
+  -moz-box-sizing: border-box;
+  -webkit-box-sizing: border-box;
+}
+nav li:last-child a {
+  border-right: 0;
+}
+nav a#pull {
+  display: none;
+}
+html {
+  -webkit-touch-callout: none;
+  -webkit-user-select: none;
+  user-select: none;
+  -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
+}
+ul {
+  /*list-style-type: none;*/
+  margin: 0;
+  padding: 0;
+  background-color: #333;
+  text-align: center;
+}
+li {
+  /*position: relative;*/
+  display: inline-block;
+}
+li a {
+  color: #ffffff;
+  text-align: center;
+  padding: 14.5px 16px;
+  text-decoration: none;
+}
+li a:hover {
+  color: #ffd400;
+  font-weight: normal;
+}
+.menu {
+  width: 100%;
+  height: 50px;
+  text-align: center;
+  max-width: 100%;
+  background-position: center;
+  background-size: cover;
+  background-color: #333333;
+  color: white;
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: 1;
+}
+</style>
